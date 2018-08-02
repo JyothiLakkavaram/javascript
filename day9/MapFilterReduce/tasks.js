1681b1e6c1681930023956849ea8ad4b9c4bf4c8
Naga Jyothi <jyothi.lakkavaram@gmail.com>
2018-08-02 14:30:20 +0530

    map, filter and reduce methods

---
+++ day9/MapFilterReduce/tasks.js
@@ -0,0 +1,29 @@
+// Durations are in minutes
+ 
+var tasks = [
+ 
+  {
+ 
+    'name'     : 'Work1',
+ 
+    'duration' : 120
+ 
+  },
+ 
+  {
+ 
+    'name'     : 'Work2',
+ 
+    'duration' : 60
+ 
+  },
+ 
+  {
+ 
+    'name'     : 'Work3',
+ 
+    'duration' : 240
+ 
+  }
+ 
+];
